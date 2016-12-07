0be42ad7e4e86d0593e2cc85d9250562e4cda871
Cheng Zhiyuan <zhiyuan.zy.cheng@gmail.com>
2016-12-07 14:55:11 +0800

    Add Week3 program file

---
+++ R-programming/week3/Programming.R
@@ -0,0 +1,36 @@
+
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  setInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       setInverse = setInverse)
+  
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- mean(data, ...)
+  x$setmean(inv)
+  inv
+}
---
+++ R-programming/week3/Quiz.R
@@ -0,0 +1,79 @@
+pollutantmean<- function(directory, pollutant = "sulfate", id = 1:332) {
+  
+  if(grep("specdata", directory) == 1) {
+    directory <- ("./specdata/")
+  }
+  
+  # initialize a vector to hold the pollutant data
+  mean_vector <- c()
+  
+  # find all files in the specdata folder
+  all_files <- as.character(list.files(directory) )
+  file_paths <- paste(directory, all_files, sep="")
+  for(i in id) {
+    current_file <- read.csv(file_paths[i], header=T, sep=",")    
+    na_removed <- current_file[!is.na(current_file[, pollutant]), pollutant]
+    mean_vector <- c(mean_vector, na_removed)
+  }
+  result <- mean(mean_vector)
+  return(round(result, 3)) 
+}
+
+
+
+complete <- function(directory, id = 1:332) {
+  
+  # set working directory
+  if(grep("specdata", directory) == 1) {
+    directory <- ("./specdata/")
+  }
+  
+  # get the length of id vector
+  id_len <- length(id)
+  complete_data <- rep(0, id_len)
+  
+  # find all files in the specdata folder
+  all_files <- as.character( list.files(directory) )
+  file_paths <- paste(directory, all_files, sep="")
+  j <- 1 
+  
+  for (i in id) {
+    current_file <- read.csv(file_paths[i], header=T, sep=",")
+    complete_data[j] <- sum(complete.cases(current_file))
+    j <- j + 1
+  }
+  result <- data.frame(id = id, nobs = complete_data)
+  return(result)
+} 
+
+
+
+
+
+corr <- function(directory, threshold = 0) {
+
+  
+  if(grep("specdata", directory) == 1) {
+    directory <- ("./specdata/")
+  }
+  # get the complete table
+  complete_table <- complete("specdata", 1:332)
+  nobs <- complete_table$nobs
+  # find the valid ids
+  ids <- complete_table$id[nobs > threshold]
+  # get the length of ids vector
+  id_len <- length(ids)
+  corr_vector <- rep(0, id_len)
+  # find all files in the specdata folder
+  all_files <- as.character( list.files(directory) )
+  file_paths <- paste(directory, all_files, sep="")
+  j <- 1
+  for(i in ids) {
+    current_file <- read.csv(file_paths[i], header=T, sep=",")
+    corr_vector[j] <- cor(current_file$sulfate, current_file$nitrate, use="complete.obs")
+    j <- j + 1
+  }
+  result <- corr_vector
+  return(result)   
+}
+
