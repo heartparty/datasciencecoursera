a26926ae07a6fbefd2f893e87925adb2aefcfaa7
Cheng Zhiyuan <zhiyuan.zy.cheng@gmail.com>
2016-12-15 14:29:36 +0800

    update the source code

--- ExData_Plotting/plot2.r
+++ ExData_Plotting/plot2.r
@@ -7,7 +7,8 @@ required <- original[original$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # makeup the plot
 datetime <- strptime(paste(required$Date, required$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalActivePower <- as.numeric(required$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
+
